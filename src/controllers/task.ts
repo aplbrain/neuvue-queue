f07e553b5b9bc5391731428c37d5503b9b9476e5
Daniel Xenes <daniel.xenes@jhuapl.edu>
2021-10-22 00:23:07 -0400

    adds metadata patching

--- src/controllers/task.ts
+++ src/controllers/task.ts
@@ -138,6 +138,31 @@ export default class TaskController extends mix(Controller).with(CRUDMixin) {
             });
         };
     }
+    
+    public setMetadata(): (req: Request, res: Response, next: Next) => void {
+        return (req: Request, res: Response, next: Next): void => {
+            const id = req.params.id;
+            const metadata = req.body;
+            
+            if (!_.isPlainObject(metadata)) {
+                return next(new BadRequestError("metadata must be a plain object"));
+            }
+            const update: { [key: string]: any } = { metadata: req.body };
+            this.model.findByIdAndUpdate(id, update, (err, old) => {
+                if (err) {
+                    if (err.name === "DocumentNotFoundError") {
+                        return next(new NotFoundError(`${req.params.id} does not exist`));
+                    } else if (err.name === "ValidationError") {
+                        return next(new BadRequestError(err.message));
+                    } else {
+                        return next(err);
+                    }
+                }
+                res.json(old);
+                res.end();
+            });
+        };
+    }
 
     public attachTo(root: string, server: Server, options?: TaskControllerOptions): void {
         if (root.endsWith("/")) {
@@ -151,6 +176,7 @@ export default class TaskController extends mix(Controller).with(CRUDMixin) {
         server.patch(`${root}/:id/priority`, this.setPriority());
         server.patch(`${root}/:id/status`, this.setStatus());
         server.patch(`${root}/:id/points`, this.appendPoint());
+        server.patch(`${root}/:id/metadata`, this.setMetadata());
         server.del(
             `${root}/:objectId/points/:pointId`, this.deactivatePoint(),
         );
