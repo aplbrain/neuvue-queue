1617b2b03cac460baf46f545828729aaf3d0c05e
Hannah Gooden <hannah.gooden@jhuapl.edu>
2022-01-14 16:25:20 -0500

    Add comment explaining what a task patch is

--- src/models/task_patch.ts
+++ src/models/task_patch.ts
@@ -1,5 +1,10 @@
 import { model, Schema } from "mongoose";
 
+// A task patch is a single action that a user makes while completing a task in ngl
+// Examples include moving the view and adding a point
+// Ngl saves task patches in viewer.differ.stack
+// Here we save them for analysis
+
 const schema = new Schema({
   task_id: {type: Schema.Types.ObjectId, ref: "Task", required: true},
   assignee: {type: String, required: true},
