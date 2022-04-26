ecb2bb0ec06f0940ed4186d13797ccfc2cf9ee86
Daniel Xenes <daniel.xenes@jhuapl.edu>
2022-04-26 13:00:39 -0400

    Revert "adds agentsjob model"
    This reverts commit 2d5a279404a6efb37616500affd7201250631cba.

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,12 +2,10 @@ import Point from "./point";
 import Task from "./task";
 import Auth from "./auth";
 import DifferStack from "./differ_stack";
-import AgentsJob from "./agents_job";
 
 export default {
     Point,
     Task,
     Auth,
-    DifferStack,
-    AgentsJob
+    DifferStack
 };
