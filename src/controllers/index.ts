3ff25236580c5783c399af5163e16ddbd1e70e76
Daniel Xenes <daniel.xenes@jhuapl.edu>
2022-04-26 12:59:26 -0400

    adds agentsjob model

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -8,12 +8,14 @@ import PointController, { PointControllerOptions } from "./point";
 import TaskController, { TaskControllerOptions } from "./task";
 import AuthController, { AuthControllerOptions } from "./auth";
 import DifferStackController, { DifferStackControllerOptions } from "./differ_stack";
+import AgentsJobController, { AgentsJobControllerOptions } from "./agents_job";
 
 export interface NeuvueQueueControllerOptions {
     point?: PointControllerOptions;
     task?: TaskControllerOptions;
     auth?: AuthControllerOptions;
     differ_stack?: DifferStackControllerOptions;
+    agents_job?: AgentsJobControllerOptions;
 }
 
 function attach(server: Server, options?: NeuvueQueueControllerOptions): void {
@@ -21,10 +23,12 @@ function attach(server: Server, options?: NeuvueQueueControllerOptions): void {
     const tasks = new TaskController(models.Task);
     const auth = new AuthController(models.Auth);
     const differ_stack = new DifferStackController(models.DifferStack);
+    const agents_job = new AgentsJobController(models.AgentsJob);
     points.attachTo("/points", server, _.get("point", options));
     tasks.attachTo("/tasks", server, _.get("task", options));
     auth.attachTo("/auth", server, _.get("auth", options));
     differ_stack.attachTo("/differstacks", server, _.get("differstack", options));
+    agents_job.attachTo("/agentsjobs", server, _.get("agentsjob", options));
 }
 
 export default {
--- src/models/index.ts
+++ src/models/index.ts
@@ -2,10 +2,12 @@ import Point from "./point";
 import Task from "./task";
 import Auth from "./auth";
 import DifferStack from "./differ_stack";
+import AgentsJob from "./agents_job";
 
 export default {
     Point,
     Task,
     Auth,
-    DifferStack
+    DifferStack,
+    AgentsJob
 };
