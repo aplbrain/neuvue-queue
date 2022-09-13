e38f9d57f33dce119bb613bacf8a8d6a8a88ef46
Daniel Xenes <daniel.xenes@jhuapl.edu>
2022-09-13 13:38:17 -0400

    changes points and agentsjob schemas

--- src/models/point.ts
+++ src/models/point.ts
@@ -17,7 +17,12 @@ const schema = new Schema({
     metadata: { type: Map, of: Schema.Types.Mixed, default: {} },
     namespace: { type: String, required: true },
     submitted: { type: Number, default: Date.now, min: 0 },
-    type: { type: String, required: true }
+    type: { type: String, required: true },
+    agents_status: {
+        default: "open",
+        enum: ["open", "membranes_in_progress", "membranes_completed", "extension_in_progress", "extension_completed"],
+        type: String,
+    }
 });
 
 const Point = model("Point", schema);
